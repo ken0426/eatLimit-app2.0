19cb08cec5c21e8f6f3b43c7d0134f0e9f2db825
ken0426 <kenkaitixyou0426@gmail.com>
2023-03-08 22:01:48 +0900

    [feat] srcフォルダを追加し各ファイルのディレクトリを変更

--- App.tsx
+++ App.tsx
@@ -1,7 +1,7 @@
 import { NavigationContainer } from '@react-navigation/native';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-import RootStackScreen from './navigation';
+import RootStackScreen from './src/navigation';
 
 export default function App() {
   return (
