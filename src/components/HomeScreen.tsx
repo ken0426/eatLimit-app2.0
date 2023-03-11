e11cccb353632f1673209786a05588695568826b
ken0426 <kenkaitixyou0426@gmail.com>
2023-03-11 23:23:51 +0900

    [feat] searchAreaの仮実装

--- src/components/HomeScreen.tsx
+++ src/components/HomeScreen.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { color } from '../../styles';
 
 const HomeScreen = () => {
   return (
     <>
+      <View style={styles.searchArea}></View>
       <View style={styles.topBar}></View>
     </>
   );
@@ -15,5 +17,12 @@ const styles = StyleSheet.create({
   topBar: {
     height: 30,
     backgroundColor: '#ffffff',
+  },
+  searchArea: {
+    height: 30,
+    backgroundColor: color.rightBlue,
+    // backgroundColor: 'red',
+    // borderColor: color.rightBlue,
+    borderWidth: 0,
   },
 });
