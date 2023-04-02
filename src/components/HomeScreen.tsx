6a9f334d993d45c5b7d0d0a7805c430ee8e64a78
ken0426 <kenkaitixyou0426@gmail.com>
2023-04-02 11:55:04 +0900

    [feat] searchバーの仮実装

--- src/components/HomeScreen.tsx
+++ src/components/HomeScreen.tsx
@@ -1,5 +1,6 @@
-import React, { useLayoutEffect, useState } from 'react';
+import React, { useState } from 'react';
 import {
+  Animated,
   FlatList,
   Keyboard,
   RefreshControl,
@@ -36,22 +37,29 @@ type Props = {
   };
 };
 
+const AnimatedFlatList = Animated.createAnimatedComponent(FlatList);
+const AnimatedTextInput = Animated.createAnimatedComponent(TextInput);
+
 const HomeScreen = ({ navigation }: Props) => {
   const [text, setText] = useState<string>('');
+  const [animatedValue] = useState(new Animated.Value(0));
+
+  const ListHeaderComponent = () => {
+    return (
+      <View style={styles.dateArea}>
+        <View>
+          <Text>{moment().format('YYYY-MM-DD')}</Text>
+        </View>
+        <TouchableOpacity style={styles.iconArea} activeOpacity={1}>
+          <FontAwesome5 name='sort-amount-down' size={20} color='black' />
+        </TouchableOpacity>
+      </View>
+    );
+  };
 
   const renderItem = ({ item, index }: any) => {
     return (
-      <View>
-        {Number(index) === 0 && (
-          <View style={styles.dateArea}>
-            <View>
-              <Text>{moment().format('YYYY-MM-DD')}</Text>
-            </View>
-            <TouchableOpacity style={styles.iconArea} activeOpacity={1}>
-              <FontAwesome5 name='sort-amount-down' size={20} color='black' />
-            </TouchableOpacity>
-          </View>
-        )}
+      <View key={Number(index)} style={{ backgroundColor: '#ffffff' }}>
         <TouchableOpacity
           style={styles.contents}
           onPress={() => {
@@ -66,23 +74,36 @@ const HomeScreen = ({ navigation }: Props) => {
       </View>
     );
   };
+  const handleScroll = Animated.event(
+    [{ nativeEvent: { contentOffset: { y: animatedValue } } }],
+    { useNativeDriver: true }
+  );
+
+  const translateY = animatedValue.interpolate({
+    inputRange: [0, 80],
+    outputRange: [0, -80],
+    extrapolate: 'clamp',
+  });
 
   return (
     <SafeAreaView>
-      <View style={{ backgroundColor: color.mainColor, height: 40 }}>
-        <TextInput
-          style={{
-            backgroundColor: '#ffffff',
-            height: 20,
-            marginHorizontal: 20,
-          }}
+      <Animated.View
+        style={[
+          { backgroundColor: color.mainColor, height: 40 },
+          { transform: [{ translateY: translateY }] },
+        ]}
+      >
+        <AnimatedTextInput
+          style={[{ backgroundColor: 'blue', marginHorizontal: 20 }]}
         />
-      </View>
-      <FlatList
+      </Animated.View>
+      <AnimatedFlatList
         data={aaa}
         renderItem={renderItem}
+        ListHeaderComponent={ListHeaderComponent}
         keyExtractor={(_, index) => index.toString()}
-        onScroll={() => Keyboard.dismiss()}
+        onScroll={handleScroll}
+        scrollEventThrottle={16}
       />
     </SafeAreaView>
   );
@@ -111,6 +132,7 @@ const styles = StyleSheet.create({
     flexDirection: 'row',
     justifyContent: 'center',
     alignItems: 'center',
+    backgroundColor: 'yellow',
   },
   iconArea: {
     position: 'absolute',
