803df42be9f0617005c874a2c6b8bad0e987246a
ken0426 <kenkaitixyou0426@gmail.com>
2023-04-02 23:31:40 +0900

    [feat] モックデータを変更

--- src/moc/index.ts
+++ src/moc/index.ts
@@ -1,6 +1,12 @@
-export const aaa = [
-  { eatName: 'たらこ' },
-  { eatName: 'いくら' },
+import { ApiData } from '../types';
+
+export const aaa: ApiData = [
+  {
+    eatName: 'たらこ',
+    image:
+      'https://p1-e6eeae93.imageflux.jp/c!/w=800,h=800,a=2/michiba-suisan/9a7ac9aa6985a45b860a.jpg',
+  },
+  { eatName: 'いくら', image: '' },
   { eatName: 'サーモン' },
   { eatName: '納豆' },
   { eatName: 'りんご' },
