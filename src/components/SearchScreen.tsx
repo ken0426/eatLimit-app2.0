36e618ecc12c11821d63cc9ed07bdbefcc15f68e
ken0426 <kenkaitixyou0426@gmail.com>
2023-04-12 23:56:11 +0900

    [feat] 検索バーのコンポーネントを作成

--- src/components/SearchScreen.tsx
+++ src/components/SearchScreen.tsx
@@ -1,8 +1,32 @@
 import React from 'react';
-import { View } from 'react-native';
+import { Text, TouchableOpacity, View } from 'react-native';
+import { color } from '../../styles';
+import Header from './molecules/Header';
+import { Ionicons } from '@expo/vector-icons';
 
-const SearchScreen = () => {
-  return <View>SearchScreen</View>;
+const SearchScreen = ({ navigation }: any) => {
+  return (
+    <Header
+      style={{
+        height: 100,
+        paddingHorizontal: 20,
+        paddingTop: 30,
+        backgroundColor: color.mainColor,
+        justifyContent: 'space-between',
+        alignItems: 'center',
+        flexDirection: 'row',
+      }}
+    >
+      <View>
+        <View>
+          <TouchableOpacity onPress={() => navigation.goBack()}>
+            <Ionicons name='chevron-back' size={24} color='black' />
+          </TouchableOpacity>
+        </View>
+        <View></View>
+      </View>
+    </Header>
+  );
 };
 
 export default SearchScreen;
