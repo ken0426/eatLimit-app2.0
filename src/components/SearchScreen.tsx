9ac180531a89c0e225b5f888455e7abf47e2cd25
ken0426 <kenkaitixyou0426@gmail.com>
2023-04-13 00:27:47 +0900

    [feat] 検索エリアのテキスト入力エリアの仮実装

--- src/components/SearchScreen.tsx
+++ src/components/SearchScreen.tsx
@@ -1,10 +1,18 @@
-import React from 'react';
-import { Text, TouchableOpacity, View } from 'react-native';
+import React, { useEffect, useRef } from 'react';
+import { Text, TextInput, TouchableOpacity, View } from 'react-native';
 import { color } from '../../styles';
 import Header from './molecules/Header';
 import { Ionicons } from '@expo/vector-icons';
 
 const SearchScreen = ({ navigation }: any) => {
+  const inputRef = useRef<TextInput>(null);
+
+  useEffect(() => {
+    if (inputRef.current) {
+      inputRef.current.focus();
+    }
+  }, []);
+
   return (
     <Header
       style={{
@@ -17,13 +25,30 @@ const SearchScreen = ({ navigation }: any) => {
         flexDirection: 'row',
       }}
     >
-      <View>
-        <View>
+      <View style={{ flexDirection: 'row', alignItems: 'center' }}>
+        <View style={{ marginRight: 10 }}>
           <TouchableOpacity onPress={() => navigation.goBack()}>
-            <Ionicons name='chevron-back' size={24} color='black' />
+            <Ionicons name='chevron-back' size={28} color='#ffffff' />
           </TouchableOpacity>
         </View>
-        <View></View>
+        <View
+          style={{
+            width: '100%',
+          }}
+        >
+          <TextInput
+            ref={inputRef}
+            placeholder='検索'
+            style={{
+              height: 40,
+              fontSize: 20,
+              borderRadius: 50,
+              paddingHorizontal: 15,
+              backgroundColor: color.TextInput,
+              width: '80%',
+            }}
+          ></TextInput>
+        </View>
       </View>
     </Header>
   );
--- styles/index.ts
+++ styles/index.ts
@@ -1,4 +1,5 @@
 /** グローバルで使用する色 */
 export const color = {
   mainColor: '#d9b89b', // rgba: 217, 184, 155
+  TextInput: '#cbaa8d',
 };
