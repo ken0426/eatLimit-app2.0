ee8de0d0051037d25248a6696b54f305a4202784
ken0426 <kenkaitixyou0426@gmail.com>
2023-04-14 00:10:19 +0900

    [refactor] リファクタリング

--- src/components/atoms/AtomSearch.tsx
+++ src/components/atoms/AtomSearch.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useRef } from 'react';
-import { TextInput, TouchableOpacity, View } from 'react-native';
+import { StyleSheet, TextInput, TouchableOpacity, View } from 'react-native';
 import { Ionicons } from '@expo/vector-icons';
 import { AntDesign } from '@expo/vector-icons';
 import { color } from '../../../styles';
@@ -14,38 +14,15 @@ const AtomSearch = ({ navigation }: any) => {
   }, []);
 
   return (
-    <View style={{ flexDirection: 'row', alignItems: 'center' }}>
+    <View style={styles.contents}>
       <View style={{ marginRight: 10 }}>
         <TouchableOpacity onPress={() => navigation.goBack()}>
           <Ionicons name='chevron-back' size={28} color='#ffffff' />
         </TouchableOpacity>
       </View>
-      <View
-        style={{
-          width: '100%',
-        }}
-      >
-        <TextInput
-          ref={inputRef}
-          placeholder='検索'
-          style={{
-            height: 40,
-            fontSize: 20,
-            borderRadius: 50,
-            paddingLeft: 30,
-            paddingRight: 20,
-            backgroundColor: color.TextInput,
-            width: '80%',
-          }}
-        />
-        <View
-          style={{
-            position: 'absolute',
-            justifyContent: 'center',
-            padding: 10,
-            height: 40,
-          }}
-        >
+      <View style={{ width: '100%' }}>
+        <TextInput ref={inputRef} placeholder='検索' style={styles.textInput} />
+        <View style={styles.icon}>
           <AntDesign name='search1' size={18} color={'#a78b74'} />
         </View>
       </View>
@@ -54,3 +31,25 @@ const AtomSearch = ({ navigation }: any) => {
 };
 
 export default AtomSearch;
+
+const styles = StyleSheet.create({
+  contents: {
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  textInput: {
+    height: 40,
+    fontSize: 20,
+    borderRadius: 50,
+    paddingLeft: 30,
+    paddingRight: 20,
+    backgroundColor: color.TextInput,
+    width: '80%',
+  },
+  icon: {
+    position: 'absolute',
+    justifyContent: 'center',
+    padding: 10,
+    height: 40,
+  },
+});
