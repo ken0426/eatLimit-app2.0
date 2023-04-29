8e0072297c5ab3343004589fb29566f6ca71941f
ken0426 <kenkaitixyou0426@gmail.com>
2023-04-30 00:35:12 +0900

    [refactor] 不要な記述を削除

--- App.tsx
+++ App.tsx
@@ -1,6 +1,5 @@
 import { NavigationContainer } from '@react-navigation/native';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
 import RootStackScreen from './src/navigation';
 
 export default function App() {
