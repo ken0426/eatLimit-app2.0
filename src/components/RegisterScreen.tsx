bab073a1322ef52b5da29c397f755a3872a0e133
ken0426 <kenkaitixyou0426@gmail.com>
2023-04-30 18:24:57 +0900

    [feat] 消費期限を決めるモーダルの仮実装
    ※リファクタリング必須

--- src/components/RegisterScreen.tsx
+++ src/components/RegisterScreen.tsx
@@ -1,5 +1,5 @@
 import { StackNavigationProp } from '@react-navigation/stack';
-import React, { FC } from 'react';
+import React, { FC, useState } from 'react';
 import { Alert, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
 import { StackPramList } from '../types';
 import MolHeader from './molecules/MolHeader';
@@ -8,12 +8,14 @@ import AtomRegister from './atoms/AtomRegister';
 import AtomFileSelect from './atoms/AtomFileSelect';
 import AtomSingleSelect from './atoms/AtomSingleSelect';
 import AtomSingleInput from './atoms/AtomSingleInput';
+import Modal from 'react-native-modal';
 
 type Props = {
   navigation: StackNavigationProp<StackPramList, 'registerScreen'>;
 };
 
 const RegisterScreen: FC<Props> = ({ navigation }) => {
+  const [isVisible, setIsVisible] = useState(false);
   return (
     <View style={{ backgroundColor: '#ffffff' }}>
       <MolHeader style={styles.header} type={'default'}>
@@ -28,8 +30,61 @@ const RegisterScreen: FC<Props> = ({ navigation }) => {
             label={'保存方法'}
             onPress={() => navigation.navigate('singleSelectScreen')}
           />
+          <AtomSingleSelect
+            label={'消費期限'}
+            onPress={() => setIsVisible(true)}
+          />
         </View>
       </View>
+      <Modal
+        animationIn={'slideInUp'}
+        isVisible={isVisible}
+        backdropOpacity={0.2}
+        style={{ justifyContent: 'flex-end', margin: 0 }}
+      >
+        <View
+          style={{
+            backgroundColor: '#ffffff',
+            width: '100%',
+            height: '30%',
+            paddingTop: 10,
+            paddingHorizontal: 10,
+          }}
+        >
+          <View
+            style={{
+              flexDirection: 'row',
+              width: '100%',
+              justifyContent: 'space-between',
+            }}
+          >
+            <TouchableOpacity onPress={() => setIsVisible(false)}>
+              <Text
+                style={{
+                  width: '100%',
+                  justifyContent: 'center',
+                  fontSize: 20,
+                  color: color.mainTextColor,
+                }}
+              >
+                キャンセル
+              </Text>
+            </TouchableOpacity>
+            <TouchableOpacity onPress={() => setIsVisible(false)}>
+              <Text
+                style={{
+                  width: '100%',
+                  justifyContent: 'center',
+                  fontSize: 20,
+                  color: color.blue,
+                }}
+              >
+                完了
+              </Text>
+            </TouchableOpacity>
+          </View>
+        </View>
+      </Modal>
     </View>
   );
 };
