1cc4e8b9ab095cc6831286903c647207c8ebc0a1
ken0426 <kenkaitixyou0426@gmail.com>
2023-04-30 14:34:49 +0900

    [refactor] 登録画面のリファクタリングとテキスト入力できる項目を追加

--- src/components/RegisterScreen.tsx
+++ src/components/RegisterScreen.tsx
@@ -1,10 +1,12 @@
 import { StackNavigationProp } from '@react-navigation/stack';
 import React, { FC } from 'react';
-import { Text, View } from 'react-native';
+import { Alert, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
 import { StackPramList } from '../types';
 import MolHeader from './molecules/MolHeader';
 import { color } from '../styles';
 import AtomRegister from './atoms/AtomRegister';
+import AtomFileSelect from './atoms/AtomFileSelect';
+import AtomSIngleInput from './atoms/AtomSIngleInput';
 
 type Props = {
   navigation: StackNavigationProp<StackPramList, 'registerScreen'>;
@@ -12,21 +14,31 @@ type Props = {
 
 const RegisterScreen: FC<Props> = ({ navigation }) => {
   return (
-    <MolHeader
-      style={{
-        height: 100,
-        paddingHorizontal: 20,
-        paddingTop: 30,
-        backgroundColor: color.mainColor,
-        justifyContent: 'center',
-        alignItems: 'center',
-        flexDirection: 'row',
-      }}
-      type={'register'}
-    >
-      <AtomRegister navigation={navigation} />
-    </MolHeader>
+    <View>
+      <MolHeader style={styles.header} type={'register'}>
+        <AtomRegister navigation={navigation} />
+      </MolHeader>
+
+      <View style={{ width: '100%', height: '100%' }}>
+        <AtomFileSelect />
+        <View style={{ padding: 10 }}>
+          <AtomSIngleInput />
+        </View>
+      </View>
+    </View>
   );
 };
 
 export default RegisterScreen;
+
+const styles = StyleSheet.create({
+  header: {
+    height: 100,
+    paddingHorizontal: 20,
+    paddingTop: 30,
+    backgroundColor: color.mainColor,
+    justifyContent: 'center',
+    alignItems: 'center',
+    flexDirection: 'row',
+  },
+});
