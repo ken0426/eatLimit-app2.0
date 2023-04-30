09d37e5e7a66132b7e441b5eede4dbd73a54de6b
ken0426 <kenkaitixyou0426@gmail.com>
2023-04-30 14:36:49 +0900

    [refactor] リファクタリング

--- src/components/atoms/AtomFileSelect.tsx
+++ src/components/atoms/AtomFileSelect.tsx
@@ -1,24 +1,14 @@
 import React from 'react';
-import { Alert, Text, TouchableOpacity, View } from 'react-native';
+import { Alert, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
 import { AntDesign } from '@expo/vector-icons';
 import { color } from '../../styles';
 
 const AtomFileSelect = () => {
   return (
-    <View
-      style={{
-        width: '100%',
-        height: 100,
-        borderBottomColor: color.detailBorderColor,
-        borderBottomWidth: 0.3,
-        justifyContent: 'center',
-        alignItems: 'center',
-        flexDirection: 'row',
-      }}
-    >
+    <View style={styles.fileSelectArea}>
       <Text
         onPress={() => Alert.alert('ファイルを選択するボタン')}
-        style={{ fontSize: 15, color: '#44d2d4', paddingRight: 5 }}
+        style={styles.fileSelectText}
       >
         ファイルを選択
       </Text>
@@ -35,3 +25,20 @@ const AtomFileSelect = () => {
 };
 
 export default AtomFileSelect;
+
+const styles = StyleSheet.create({
+  fileSelectArea: {
+    width: '100%',
+    height: 100,
+    borderBottomColor: color.detailBorderColor,
+    borderBottomWidth: 0.3,
+    justifyContent: 'center',
+    alignItems: 'center',
+    flexDirection: 'row',
+  },
+  fileSelectText: {
+    fontSize: 15,
+    color: '#44d2d4',
+    paddingRight: 5,
+  },
+});
