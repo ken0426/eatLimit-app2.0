d1d24b2923f8e8256124823aa552d74dd0b3123d
ken0426 <kenkaitixyou0426@gmail.com>
2023-05-11 23:43:56 +0900

    [fix] アトムボタンのスタイルの渡し方の修正

--- src/components/atoms/AtomButton.tsx
+++ src/components/atoms/AtomButton.tsx
@@ -1,18 +1,52 @@
 import React, { FC } from 'react';
-import { StyleProp, Text, TextStyle, TouchableOpacity } from 'react-native';
+import { StyleSheet, Text, TouchableOpacity } from 'react-native';
 
 type Props = {
   onPress: () => void;
-  style: StyleProp<TextStyle>;
   label: string;
+  color: string;
+  fontSize: number;
+  backgroundColor: string;
+  width: number;
+  borderColor?: string;
+  borderWidth?: number;
 };
 
-const AtomButton: FC<Props> = ({ onPress, style, label }) => {
+const AtomButton: FC<Props> = ({
+  onPress,
+  fontSize,
+  color,
+  backgroundColor,
+  width,
+  label,
+  borderColor,
+  borderWidth = 0,
+}) => {
   return (
-    <TouchableOpacity onPress={onPress}>
-      <Text style={style}>{label}</Text>
+    <TouchableOpacity
+      style={[
+        styles.contents,
+        {
+          backgroundColor,
+          width,
+          borderColor,
+          borderWidth,
+        },
+      ]}
+      onPress={onPress}
+      activeOpacity={1}
+    >
+      <Text style={{ fontSize, color }}>{label}</Text>
     </TouchableOpacity>
   );
 };
 
 export default AtomButton;
+
+const styles = StyleSheet.create({
+  contents: {
+    alignItems: 'center',
+    borderRadius: 20,
+    paddingVertical: 2,
+  },
+});
