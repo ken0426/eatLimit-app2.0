c7181978f9e89c601e4fc96f44c6faed9bc438fa
ken0426 <kenkaitixyou0426@gmail.com>
2023-06-25 13:49:01 +0900

    [fix] 不要なコメントを削除

--- src/contents/index.ts
+++ src/contents/index.ts
@@ -4,9 +4,7 @@ import { KeepData, ManagementData, ModalButton, SettingData } from '../types';
 /** 検索バー */
 export const SEARCH_BAR_HIGHT = 40;
 
-/** フォントサイズ
- * @param {number} SEARCH_BAR_TEXT 検索バーのフォントサイズ
- */
+/** 検索バーのフォントサイズ */
 export const SEARCH_BAR_TEXT = 20;
 
 /** カメラへの権限メッセージ */
