5590d6df644e1a61c3f36e8c379b83e9f4dc19d3
ken0426 <kenkaitixyou0426@gmail.com>
2023-07-27 23:52:08 +0900

    [feat] 入力項目を追加

--- src/components/LoginScreen.tsx
+++ src/components/LoginScreen.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
-import { Text, View } from 'react-native';
+import { Text, TextInput, View } from 'react-native';
 
 const LoginScreen = () => {
   return (
     <View>
       <Text>LoginScreen</Text>
+      <TextInput />
     </View>
   );
 };
