e242b6afbbe15bc8c35f3fdf7ce72c30baf55f2c
ken0426 <kenkaitixyou0426@gmail.com>
2023-08-09 23:33:06 +0900

    [fix] 仮データを追加

--- src/moc/index.ts
+++ src/moc/index.ts
@@ -135,4 +135,10 @@ export const data: ApiData[] = [
     management: '消費期限',
     keep: '常温',
   },
+  {
+    eatName: '画像なし',
+    date: '2023-08-09',
+    management: '消費期限',
+    keep: '常温',
+  },
 ];
