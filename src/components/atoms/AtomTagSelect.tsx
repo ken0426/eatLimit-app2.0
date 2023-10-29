cd98456aa2e8b52299d00d0d9261f08a25900f3a
ken0426 <kenkaitixyou0426@gmail.com>
2023-10-29 23:09:25 +0900

    [feat] タグを含んだ場合のUIを追加

--- src/components/atoms/AtomTagSelect.tsx
+++ src/components/atoms/AtomTagSelect.tsx
@@ -1,11 +1,23 @@
 import React from 'react';
-import { StyleSheet, Text, TouchableOpacity } from 'react-native';
-import { COLORS, FONTSIZE, INPUT_HEIGHT } from '../../styles';
+import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import { MaterialIcons } from '@expo/vector-icons';
+import { COLORS, FONTSIZE, INPUT_HEIGHT, SIZE } from '../../styles';
 
 const AtomTagSelect = () => {
   return (
     <TouchableOpacity onPress={() => {}} style={styles.itemArea}>
       <Text style={styles.label}>タグ：</Text>
+      <View style={styles.displayTagAra}>
+        {/* TODO ここはループさせ、タグが2つ以上の場合は最後のタグ以外はカンマを付ける */}
+        <Text style={styles.text}>タグテスト</Text>
+      </View>
+      <View>
+        <MaterialIcons
+          name='keyboard-arrow-down'
+          size={20}
+          color={COLORS.TEXT_COLOR}
+        />
+      </View>
     </TouchableOpacity>
   );
 };
@@ -15,7 +27,7 @@ export default AtomTagSelect;
 const styles = StyleSheet.create({
   itemArea: {
     flexDirection: 'row',
-    height: INPUT_HEIGHT,
+    minHeight: INPUT_HEIGHT,
     alignItems: 'center',
     borderBottomWidth: 0.3,
     borderBottomColor: COLORS.DETAIL_BORDER,
@@ -24,5 +36,17 @@ const styles = StyleSheet.create({
     fontSize: FONTSIZE.SIZE18PX,
     color: COLORS.TEXT_LABEL,
     fontWeight: '400',
+  },
+  displayTagAra: {
+    alignItems: 'flex-end',
+    justifyContent: 'flex-end',
+    flex: 1,
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+  },
+  text: {
+    fontSize: FONTSIZE.SIZE18PX,
+    fontWeight: 'bold',
+    paddingVertical: SIZE.BASE_WP,
   },
 });
