5706ef3bd3d72a31504190c32bb1b13bd090f514
ken0426 <kenkaitixyou0426@gmail.com>
2023-11-05 00:20:07 +0900

    [refactor] 必要なコメントを追加

--- src/redux/slices/commonRegisterSlice.ts
+++ src/redux/slices/commonRegisterSlice.ts
@@ -26,9 +26,11 @@ export const commonRegisterSlice = createSlice({
   name: 'commonRegister',
   initialState: commonRegisterState,
   reducers: {
+    /** 登録やコピーで項目のデータを保持 */
     setRegisterData: (state, { payload }) => {
       state.registerData = payload;
     },
+    /** 変更で項目のデータを保持 */
     setUpdateRegisterData: (state, { payload }) => {
       state.updateRegisterData = payload;
     },
