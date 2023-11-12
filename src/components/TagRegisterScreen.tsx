f4ff1c9094f341fe04eb4a766a80bec7bc055cd3
ken0426 <kenkaitixyou0426@gmail.com>
2023-11-13 00:39:51 +0900

    [fix] タグの登録画面で画面表示時にキーボードがはじめから表示されない不具合の修正
    また、空文字の場合登録できないように修正

--- src/components/TagRegisterScreen.tsx
+++ src/components/TagRegisterScreen.tsx
@@ -1,5 +1,12 @@
 import React, { useState } from 'react';
-import { StyleSheet, Text, TextInput, View } from 'react-native';
+import {
+  Keyboard,
+  StyleSheet,
+  Text,
+  TextInput,
+  TouchableWithoutFeedback,
+  View,
+} from 'react-native';
 import { useNavigation } from '@react-navigation/native';
 import uuid from 'react-native-uuid';
 import { widthPercentageToDP as wp } from 'react-native-responsive-screen';
@@ -18,57 +25,62 @@ const TagRegisterScreen = () => {
   const [text, setText] = useState('');
 
   const onRightPress = () => {
-    const tagListCopy = [...tagList];
+    if (text.length) {
+      const tagListCopy = [...tagList];
 
-    // TODO ここでAPIを叩き、DBへの保存が完了したらreduxにデータを保存する
-    dispatch(
-      setTagList([...tagListCopy, { id: String(uuid.v4()), name: text }])
-    );
-    navigation.goBack();
+      // TODO ここでAPIを叩き、DBへの保存が完了したらreduxにデータを保存する
+      dispatch(
+        setTagList([...tagListCopy, { id: String(uuid.v4()), name: text }])
+      );
+      navigation.goBack();
+    }
   };
 
   return (
-    <View style={styles.screen}>
-      <MolHeader style={styles.header} type={HEADER_TYPE.DEFAULT}>
-        <AtomSettingRegister
-          title={'タグ作成'}
-          isRightText={BUTTON_TEXT.DECISION}
-          isRightButton={true}
-          onRightPress={onRightPress}
-        />
-      </MolHeader>
+    <TouchableWithoutFeedback onPress={() => Keyboard.dismiss()}>
+      <View style={styles.screen}>
+        <MolHeader style={styles.header} type={HEADER_TYPE.DEFAULT}>
+          <AtomSettingRegister
+            title={'タグ作成'}
+            isRightText={BUTTON_TEXT.DECISION}
+            isRightButton={true}
+            onRightPress={onRightPress}
+          />
+        </MolHeader>
 
-      <View style={styles.contents}>
-        <View style={styles.label}>
-          <Text style={styles.labelText}>名前</Text>
+        <View style={styles.contents}>
+          <View style={styles.label}>
+            <Text style={styles.labelText}>名前</Text>
+          </View>
+          <TextInput
+            autoFocus={true}
+            placeholder={'タグ名を入力'}
+            maxLength={20}
+            style={styles.textInput}
+            value={text}
+            onChangeText={(e) => setText(e)}
+          />
+          {text.length === 20 && (
+            <Text style={styles.errorText}>
+              ※入力できる文字数は20文字までです
+            </Text>
+          )}
         </View>
-        <TextInput
-          placeholder={'タグ名を入力'}
-          maxLength={20}
-          style={styles.textInput}
-          value={text}
-          onChangeText={(e) => setText(e)}
-        />
-        {text.length === 20 && (
-          <Text style={styles.errorText}>
-            ※入力できる文字数は20文字までです
-          </Text>
-        )}
-      </View>
 
-      <View style={styles.buttonArea}>
-        <AtomButton
-          onPress={onRightPress}
-          buttonText={BUTTON_TEXT.DECISION}
-          color={COLORS.WHITE}
-          fontSize={FONTSIZE.SIZE25PX}
-          backgroundColor={COLORS.BLUE}
-          width={wp('90%')}
-          buttonStyle={styles.registerButton}
-          textStyle={styles.registerButtonText}
-        />
+        <View style={styles.buttonArea}>
+          <AtomButton
+            onPress={onRightPress}
+            buttonText={BUTTON_TEXT.DECISION}
+            color={COLORS.WHITE}
+            fontSize={FONTSIZE.SIZE25PX}
+            backgroundColor={COLORS.BLUE}
+            width={wp('90%')}
+            buttonStyle={styles.registerButton}
+            textStyle={styles.registerButtonText}
+          />
+        </View>
       </View>
-    </View>
+    </TouchableWithoutFeedback>
   );
 };
 
@@ -92,7 +104,7 @@ const styles = StyleSheet.create({
   },
   contents: {
     marginHorizontal: SIZE.BASE_WP * 8,
-    marginTop: SIZE.BASE_WP * 50,
+    paddingTop: SIZE.BASE_WP * 50,
   },
   label: {
     marginBottom: SIZE.BASE_WP * 5,
