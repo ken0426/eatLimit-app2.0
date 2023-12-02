49eceb8598e1f65fb9626c462c0f276661025e2b
ken0426 <kenkaitixyou0426@gmail.com>
2023-12-03 08:20:05 +0900

    [feat] 詳細画面のメモ項目でメモが長い場合、アコーディオンで全文の表示ができるように実装

--- src/components/atoms/AtomDetailMemo.tsx
+++ src/components/atoms/AtomDetailMemo.tsx
@@ -1,22 +1,52 @@
 import React, { FC } from 'react';
-import { ScrollView, StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View } from 'react-native';
+import Accordion from '@gapur/react-native-accordion';
 import { COLORS, FONTSIZE, SIZE } from '../../styles';
 import { heightPercentageToDP } from 'react-native-responsive-screen';
 import { ApiData } from '../../types';
+import AtomMemoAccordionRenderItem from './AtomMemoAccordionRenderItem';
 
 type Props = {
   item: ApiData;
 };
 
 const AtomDetailMemo: FC<Props> = ({ item }) => {
+  const [isOpen, setIsOpen] = React.useState(false);
+
+  const renderHeader = () => {
+    return (
+      <AtomMemoAccordionRenderItem
+        item={item}
+        numberOfLines={7}
+        iconName={'keyboard-arrow-down'}
+      />
+    );
+  };
+
   return (
     <View style={styles.contents}>
       <Text style={styles.label}>メモ</Text>
-      <View style={styles.memoArea}>
-        <ScrollView style={styles.scrollArea}>
-          <Text style={styles.memoText}>{item.memo}</Text>
-        </ScrollView>
-      </View>
+
+      <Accordion
+        showButton
+        headerTitle={''}
+        headerTitleStyle={styles.memoText}
+        headerStyle={{ display: isOpen ? 'none' : 'flex' }}
+        renderHeader={renderHeader}
+        renderButtonContent={(e) => {
+          setTimeout(() => {
+            setIsOpen(e);
+          }, 150);
+          return <></>;
+        }}
+        style={styles.accordionStyle}
+        buttonStyle={styles.accordionButton}
+      >
+        <AtomMemoAccordionRenderItem
+          item={item}
+          iconName={'keyboard-arrow-up'}
+        />
+      </Accordion>
     </View>
   );
 };
@@ -27,14 +57,15 @@ const styles = StyleSheet.create({
   contents: {
     marginTop: SIZE.BASE_WP * 3,
     backgroundColor: COLORS.WHITE,
-    paddingHorizontal: SIZE.BASE_WP * 3,
     shadowOffset: { width: 0, height: 2 }, // ここで影のオフセットを設定
     shadowOpacity: 0.2, // 影の透明度
     shadowRadius: 1, // 影のぼかしの範囲
+    elevation: 3,
   },
   label: {
     fontSize: FONTSIZE.SIZE20PX,
-    paddingVertical: SIZE.BASE_WP * 3,
+    paddingTop: SIZE.BASE_WP * 3,
+    paddingHorizontal: SIZE.BASE_WP * 3,
   },
   memoArea: {
     paddingBottom: SIZE.BASE_WP * 6,
@@ -44,5 +75,17 @@ const styles = StyleSheet.create({
   },
   memoText: {
     fontSize: FONTSIZE.SIZE20PX,
+  },
+  accordionStyle: {
+    borderRadius: 0,
+    shadowOpacity: 0,
+    shadowRadius: 0,
+    borderColor: 'transparent',
+    backgroundColor: COLORS.WHITE,
+    elevation: 0,
+  },
+  accordionButton: {
+    flex: 1,
+    padding: SIZE.BASE_WP * 2,
   },
 });
---
+++ src/components/atoms/AtomMemoAccordionRenderItem.tsx
@@ -0,0 +1,50 @@
+import React, { FC } from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import SvgIcon from '../../images/SvgIcon';
+import { COLORS, FONTSIZE, SIZE } from '../../styles';
+import { ApiData } from '../../types';
+
+type Props = {
+  item: ApiData;
+  numberOfLines?: number;
+  iconName: 'keyboard-arrow-down' | 'keyboard-arrow-up';
+};
+
+const AtomMemoAccordionRenderItem: FC<Props> = ({
+  item,
+  numberOfLines,
+  iconName,
+}) => {
+  return (
+    <View>
+      <Text numberOfLines={numberOfLines} style={styles.memoText}>
+        {item.memo}
+      </Text>
+      <View style={styles.accordionIconArea}>
+        <SvgIcon
+          type={'materialIcons'}
+          name={iconName}
+          size={30}
+          color={COLORS.BLACK}
+        />
+      </View>
+    </View>
+  );
+};
+
+export default AtomMemoAccordionRenderItem;
+
+const styles = StyleSheet.create({
+  memoText: {
+    fontSize: FONTSIZE.SIZE20PX,
+  },
+  accordionIconArea: {
+    position: 'absolute',
+    flex: 1,
+    bottom: -SIZE.BASE_WP * 7,
+    left: 0,
+    right: 0,
+    alignItems: 'center',
+    height: SIZE.BASE_WP * 8,
+  },
+});
