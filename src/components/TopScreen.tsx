35270c7b6c22c5020c7f55d693f45cff3ee120b6
ken0426 <kenkaitixyou0426@gmail.com>
2024-01-05 20:49:33 +0900

    [refactor] 不要な記述を削除

--- src/components/TopScreen.tsx
+++ src/components/TopScreen.tsx
@@ -1,18 +1,10 @@
 import React from 'react';
-import {
-  Alert,
-  ImageBackground,
-  SafeAreaView,
-  StyleSheet,
-  Text,
-  TouchableOpacity,
-  View,
-} from 'react-native';
+import { ImageBackground, StyleSheet, View } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
 import { StackNavigationProp } from '@react-navigation/stack';
 import { StackPramList } from '../types';
 import AtomAuthButton from './atoms/AtomAuthButton';
-import { COLORS, FONTSIZE, SIZE } from '../styles';
+import { COLORS, SIZE } from '../styles';
 
 const TopScreen = () => {
   const navigation = useNavigation<StackNavigationProp<StackPramList>>();
@@ -29,21 +21,7 @@ const TopScreen = () => {
           textColor={COLORS.SIGN_IN_BUTTON}
           text={'はじめる'}
         />
-        {/* <View style={{ marginBottom: 10 }} />
-        <AtomAuthButton
-          onPress={() => navigation.navigate('loginScreen', { isLogin: false })}
-          backgroundColor={'#eeeeee'}
-          textColor={'#0c0c0c'}
-          text={'新規登録'}
-        /> */}
       </View>
-      {/* <TouchableOpacity
-        onPress={() => Alert.alert('準備中')}
-        style={styles.service}
-      >
-        <Text>利用規約</Text>
-      </TouchableOpacity> */}
-      {/* </View> */}
     </ImageBackground>
   );
 };
@@ -58,17 +36,6 @@ const styles = StyleSheet.create({
   contents: {
     alignItems: 'center',
     justifyContent: 'center',
-    // height: SIZE.BASE_WP * 70,
     padding: SIZE.BASE_WP * 6,
-  },
-  title: {
-    marginBottom: SIZE.BASE_HP * 3.5,
-  },
-  titleText: {
-    fontSize: FONTSIZE.SIZE20PX,
-  },
-  service: {
-    // marginTop: SIZE.BASE_HP * 3.5,
-    alignItems: 'center',
   },
 });
