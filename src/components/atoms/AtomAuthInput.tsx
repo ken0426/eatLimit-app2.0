3824f50bdd060564df2b63e81a6de12dbf440a0d
ken0426 <kenkaitixyou0426@gmail.com>
2024-02-02 02:14:20 +0900

    [refactor] 不要な記述を削除

--- src/components/atoms/AtomAuthInput.tsx
+++ src/components/atoms/AtomAuthInput.tsx
@@ -1,4 +1,4 @@
-import React, { FC, useEffect, useState } from 'react';
+import React, { FC, useState } from 'react';
 import { KeyboardTypeOptions, StyleSheet, Text, View } from 'react-native';
 import { COLORS, FONTSIZE, SIZE } from '../../styles';
 import { TextInput } from 'react-native-paper';
